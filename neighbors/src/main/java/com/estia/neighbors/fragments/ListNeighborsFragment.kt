070d4920c99c1986bbafd67a714e31838580d0f4
HAKAOUI <80093307+HAKAOUI@users.noreply.github.com>
2021-11-06 15:29:44 +0100

    last commit

---
+++ neighbors/src/main/java/com/estia/neighbors/fragments/ListNeighborsFragment.kt
@@ -0,0 +1,74 @@
+package com.estia.neighbors.fragments
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.estia.neighbors.fragments.adapters.ListNeighborHandler
+import com.estia.neighbors.fragments.adapters.ListNeighborsAdapter
+import com.estia.neighbors.fragments.data.NeighborRepository
+import com.estia.neighbors.fragments.models.Neighbor
+import com.gmail.hakaoui.neighbors.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListNeighborsFragment : Fragment(), ListNeighborHandler {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var addNeighbor: FloatingActionButton
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.list_neighbors_fragment, container, false)
+        (activity as? NavigationListener)?.updateTitle(R.string.liste_neighbor_toolbar_name)
+        recyclerView = view.findViewById(R.id.neighbors_list)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        addNeighbor = view.findViewById(R.id.addNeighbor)
+        addNeighbor.setOnClickListener {
+            (activity as? NavigationListener)?.showFragment(AddNeighbourFragment())
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        refresh()
+    }
+
+    override fun onDeleteNeibor(neighbor: Neighbor) {
+        val alert = AlertDialog.Builder(context)
+        alert.setTitle("Confirmation")
+            .setMessage("Do you want to delete this user?")
+
+        alert.setPositiveButton(android.R.string.ok) { dialog, _ ->
+            NeighborRepository.getInstance().deleteNeighbour(neighbor)
+            refresh()
+        }
+
+        alert.setNegativeButton(android.R.string.cancel) { dialog, _ ->
+            Toast.makeText(
+                context,
+                android.R.string.cancel, Toast.LENGTH_SHORT
+            ).show()
+        }
+        alert.create().show()
+    }
+
+    private fun refresh() {
+        val neighbors = NeighborRepository.getInstance().getNeighbours()
+        val adapter = ListNeighborsAdapter(neighbors, this)
+        recyclerView.adapter = adapter
+    }
+}
---
+++ neighbors/src/main/java/com/gmail/hakaoui/neighbors/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.gmail.hakaoui.neighbors
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.estia.neighbors.fragments.ListNeighborsFragment
+import com.estia.neighbors.fragments.NavigationListener
+
+class MainActivity : AppCompatActivity(), NavigationListener {
+    private lateinit var toolbar: Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        showFragment(ListNeighborsFragment())
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+        }.commit()
+    }
+
+    override fun updateTitle(title: Int) {
+        toolbar.setTitle(title)
+    }
+}
